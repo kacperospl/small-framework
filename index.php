950e33c2a9231858da9aef79aaca887b2f8dc4c5
Kacper Smal <kacpersmal@gmail.com>
2018-02-09 14:53:35 +0100

    benchmark is red if time is longer than 1 sec

--- index.php
+++ index.php
@@ -21,17 +21,33 @@ $app->router->dispatch();
 $END_TIME = microtime(TRUE);
 
 $benchmark = $END_TIME - $START_TIME;
-$config = new \App\Config();
-if($config->other['benchmark']){
-  echo '
-    <style>
-      .BenchmarkRes {
-        position:absolute;
-        top:0;
-        left:0;
-        background-color:blue;
-      }
-    </style>
-    <p class="BenchmarkRes"> '.$benchmark.' </p>
-  ';
+
+printBenchmark($benchmark);
+
+
+
+function printBenchmark($benchmark)
+{
+  $config = new \App\Config();
+  if($config->other['benchmark']){
+    echo '
+      <style>
+        .BenchmarkRes {
+          position:absolute;
+          top:0;
+          left:0;
+
+          ';
+
+          if($benchmark>1){
+            echo 'background-color:red;';
+          }else {
+            echo 'background-color:blue;';
+          }
+      echo '
+        }
+      </style>
+      <p class="BenchmarkRes"> '.$benchmark.' </p>
+    ';
+  }
 }
